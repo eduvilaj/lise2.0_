bd7b6d78c670b1377ce820ff386d5c67af7741ed
eduvilaj <59335450+eduvilaj@users.noreply.github.com>
2019-12-29 13:52:11 -0500

    modification des liens v2

--- js/ns1.js
+++ js/ns1.js
@@ -1,13 +1,13 @@
 function Afficher() {
   //1 - recuperer les valeurs selectionnees dans les 3 champs
 
-  var Niv_ = Ns1;
+  var Niv_ = 'Ns1';
   var Mat_ = document.getElementById('Matiere');
   var Les_ = document.getElementById('Lesson');
   var info_ = document.getElementById('info');
 
-  var ch_ = Niv_.value + '_' + Mat_.value + '_' + Les_.value + '.pdf';
-
+  //var ch_ = Niv_.value + '_' + Mat_.value + '_' + Les_.value + '.pdf';
+  var ch_ = Niv_ + '_' + Mat_.value + '_' + Les_.value + '.pdf';
   //info_.innerHTML="Tu viens de selectionner le fichier :"+ch_;
   // alert("Tu viens de selectionner le fichier "+ch_);
   PDFObject.embed('ressources/' + ch_, '#pdf_view');
