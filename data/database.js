2b3355574790cd89c46ce2ebf229c6005f9ee055
eduvilaj <59335450+eduvilaj@users.noreply.github.com>
2020-01-09 10:39:30 -0500

    Ajouts du fichier database.js

---
+++ data/database.js
@@ -0,0 +1,45 @@
+const data = {
+  lise2_0: {
+    Ns1: {
+      pwogram: 'Mathematiques',
+      L1: [
+        {
+          No_Question: 1,
+          Question: "Quelle est la Superficie d'Haiti ?",
+          Vrai_reponse: 'A',
+          reponses: [
+            { Lettre: 'A', possible_reponse: '27 750 kilometre carree' },
+            { Lettre: 'B', possible_reponse: '125 000 kilometre carree' },
+            { Lettre: 'C', possible_reponse: '129 000 kilometre carree' },
+            { Lettre: 'D', possible_reponse: '127 000 kilometre carree' }
+          ]
+        },
+        {
+          No_Question: 2,
+          Question: "Quelle est la capitale d'Haiti?",
+          Vrai_reponse: 'B',
+          reponses: [
+            { Lettre: 'A', possible_reponse: 'Cayes' },
+            { Lettre: 'B', possible_reponse: 'Port-au-Prince' },
+            { Lettre: 'C', possible_reponse: 'Jacmel' },
+            { Lettre: 'D', possible_reponse: 'Petion-Ville' }
+          ]
+        },
+        {
+          No_Question: 3,
+          Question: "En Quelle annee , HAiti a t'elle independante?",
+          Vrai_reponse: 'C',
+          reponses: [
+            { Lettre: 'A', possible_reponse: '2004' },
+            { Lettre: 'B', possible_reponse: '1992' },
+            { Lettre: 'C', possible_reponse: '1804' },
+            { Lettre: 'D', possible_reponse: '2020' }
+          ]
+        }
+      ]
+    }
+  }
+};
+
+//return "Technology";
+// export default { data };
